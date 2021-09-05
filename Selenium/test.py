71c9f7de31cc41b9f98d17583cc3b2961c71679a
Artur Bardziński <artur.bardzinski@icloud.com>
2021-09-05 23:30:32 +0200

    Selenium - first try

---
+++ Selenium/test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path='../Selenium/chromedriver')
+
+driver.get('http://mbank.pl')
+title = driver.title
+print(title)
+time.sleep(3)
+login = driver.find_element_by_xpath('//*[@id="log-in"]/a[2]')
+login.click()
