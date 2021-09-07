20bb1c7f7ff0132bb1b9761c626147a6d91aa8d9
Artur <64813722+arturbardzinski@users.noreply.github.com>
2021-09-07 17:38:43 +0200

    Create exercise_book_12.py

---
+++ exercise_book_12.py
@@ -0,0 +1,42 @@
+import math
+
+'''
+
+class Apple():
+  def __init__ (self, age, color, taste, weight):
+    self.age = age
+    self.color = color
+    self.taste = taste
+    self.weight = weight
+
+a1 = Apple(2, 'blue', 'good', 40)
+
+print(a1.age)
+
+
+
+
+class Circle():
+  def __init__(self, r):
+    self.r = r
+    
+
+  def area(self):
+    return math.pi * self.r ** 2
+  
+circle1 = Circle(5)
+print(circle1.area())
+
+
+'''
+
+class Triangle():
+  def __init__(self, a, h):
+    self.a = a
+    self.h = h
+
+  def area (self):
+    return (self.a * self.h) / 2
+
+triangle1 = Triangle(5,2)
+print(triangle1.area())
